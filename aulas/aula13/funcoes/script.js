37101050b78b27d87736915597d7283daf1cab31
DANIELLE YASMIN DIAS PADILHA <87512300034@senacrs.edu.br>
2025-04-16 17:31:55 -0300

    Adicionando conteudos da aula 16.04.25

---
+++ aulas/aula13/funcoes/script.js
@@ -0,0 +1,7 @@
+let banners = ['./img/davi.jpeg', './img/pascoa.png'];
+let cont = 0;
+function trocaBanner() {
+    cont = (cont + 1) % 2;
+    document.querySelector(".destaque img").src = banners[cont];
+}
+setInterval(trocaBanner, 1000);
