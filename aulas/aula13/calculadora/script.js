7ec1f4964be6c943312fe6820abfb63dd0878d15
DANIELLE YASMIN DIAS PADILHA <87512300034@senacrs.edu.br>
2025-04-22 17:31:54 -0300

    Adicionando conteudo da aula 22.04.25

--- aulas/aula13/calculadora/script.js
+++ aulas/aula13/calculadora/script.js
@@ -1,5 +1,6 @@
 let textValue = '';
 let input = document.querySelector("#inputText");
+const LIMITE_CARACTERES = 12;
 
 function addNumber(number) {
     textValue += number;
@@ -7,8 +8,10 @@ function addNumber(number) {
 }
 
 function addOperator(operator) {
-    textValue += operator;
-    updateText();
+    if (textValue.length < LIMITE_CARACTERES && (operator !== '%' || !textValue.endsWith('%'))) {
+        textValue += operator;
+        updateText();
+    }
 }
 
 function addDecimal() {
@@ -29,11 +32,22 @@ function updateText() {
 
 function calculate() {
     try {
-        textValue = eval(textValue).toString();
+        let conta = textValue.replace(/(\d+)%(\d+)/g, '($1/100)*$2');
+        let resultado = eval(conta);
+        if (!Number.isInteger(resultado)) {
+            resultado = resultado.toFixed(2);
+        }
+
+        textValue = resultado.toString();
         updateText();
     } catch {
         textValue = 'Erro';
         updateText();
         setTimeout(clearText, 1000);
     }
+}
+
+function deleteLast() {
+    textValue = textValue.slice(0, -1);
+    updateText();
 }
